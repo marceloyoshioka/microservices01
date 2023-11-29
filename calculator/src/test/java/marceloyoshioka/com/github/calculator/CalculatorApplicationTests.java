9ddfb003ca900b0494976cd8a55c886c75449d99
Peterson Marcelo Santos Yoshioka <pmsyoshioka@gmail.com>
2023-11-29 17:32:14 -0300

    Initial commit

---
+++ calculator/src/test/java/marceloyoshioka/com/github/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package marceloyoshioka.com.github.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
